5278d792ef57517c8f5d0cef2aa54e5035c12281
techiemokhele <neomokhele23@gmail.com>
2022-02-19 19:00:51 +0200

    commit to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
+const alanKey = "";
+
+export const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mokhele AI News Application</h1>
     </div>
   );
-}
+};
 
 export default App;
