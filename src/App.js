be9d7a2de11b248ad286c159f774f38ea9fa99a8
techiemokhele <neomokhele23@gmail.com>
2022-02-20 10:28:06 +0200

    Project commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,98 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-const alanKey = "";
+import logo from "./images/logo.png";
+import { NewsCards, Modal } from "./components";
+import useStyles from "./styles";
 
-export const App = () => {
+const alanKey =
+  "b6989ca4a02ac59d25577051df4b03042e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
     });
   }, []);
 
   return (
     <div>
-      <h1>Mokhele AI News Application</h1>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img src="./images/logo.png" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="https://www.linkedin.com/in/neo-mokhele-458188188/"
+            >
+              {" "}
+              Neo Mokhele
+            </a>{" "}
+            -
+            <a className={classes.link} href="mailto:neomokhele23@gmail.com">
+              {" "}
+              Contact Me
+            </a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="logo" />
+        </div>
+      ) : null}
     </div>
   );
 };
